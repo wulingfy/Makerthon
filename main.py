f18119dfb1c512c43617a21d0fa0b53d161e2137
Hoang Pham <phamhoang19082009@gmail.com>
2025-04-22 11:01:15 +0700

    Install requirements' lib

--- main.py
+++ main.py
@@ -1,24 +1,6 @@
 import speaker 
 import processing
 import recording
-
-Process = True
-keyword = [
-    "see you again"
-    "bye"
-    "quit"
-]
-
-def add(full_text,text):
-    full_text = full_text + ' ' + text
-    return
-
-def turn_off(script):
-    for key in keyword:
-        if key in script:
-            return False
-    return True        
-
 import keyboard
 
 
